578ca39ab8abb72ba0c4031ec8dd342b8864477d
srilakshmi kongala <kongalasriakshmi@gmail.com>
2024-02-11 13:23:19 +0530

    TEST-01
    Signed-off-by: srilakshmi kongala <kongalasriakshmi@gmail.com>

---
+++ Section B - Ans 2/calculateArea.js
@@ -0,0 +1,12 @@
+//Implement a JavaScript function called calculateArea that takes two parameters (length and width) and returns the area of a rectangle.
+
+
+function calculateArea(length, width) {
+    return length*width;
+  }
+  let area = calculateArea(4,5);
+  
+  console.log(area);
+  
+
+  
